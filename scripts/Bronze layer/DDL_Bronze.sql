3e8c0902b864bb8865428787ab88faa3c8bdf61c
Himesh Jana <himesh9991@gmail.com>
2026-07-17 21:00:07 +0530

    Update DDL_Bronze.sql

--- scripts/Bronze layer/DDL_Bronze.sql
+++ scripts/Bronze layer/DDL_Bronze.sql
@@ -14,7 +14,7 @@ cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
 cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
-cst_create_date DATE
+cst_create_date INT
 )
 go
 
@@ -26,8 +26,8 @@ prd_key NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(50),
-prd_start_dt DATE,
-prd_end_dt DATE
+prd_start_dt INT,
+prd_end_dt INT
 )
 go
 
@@ -38,8 +38,8 @@ sls_ord_num NVARCHAR(50),
 sls_prd_key NVARCHAR(50),
 sls_cust_id INT,
 sls_order_dt INT,
-sls_ship_dt DATE,
-sls_due_dt DATE,
+sls_ship_dt INT,
+sls_due_dt INT,
 sls_sales INT,
 sls_quantity INT,
 sls_price INT
@@ -50,7 +50,7 @@ go
 CREATE TABLE bronze.erp_cust_az12
 (
 cid NVARCHAR(50),
-bdate DATE,
+bdate INT,
 gen NVARCHAR(50)
 )
 go
