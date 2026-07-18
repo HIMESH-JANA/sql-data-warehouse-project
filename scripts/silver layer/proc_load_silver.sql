e525ce3a3517f0c690a69a3dfe2952fb7861dd78
Himesh Jana <himesh9991@gmail.com>
2026-07-18 23:50:54 +0530

    Update proc_load_silver.sql

--- scripts/silver layer/proc_load_silver.sql
+++ scripts/silver layer/proc_load_silver.sql
@@ -84,12 +84,21 @@ BEGIN
 			ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
 		END AS sls_due_dt,
 		CASE 
-			WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales!=sls_quantity*ABS(sls_price) THEN sls_quantity*ABS(sls_price)
+			WHEN sls_quantity IS NULL OR sls_quantity <= 0 THEN 1   -- or ABS(sls_quantity), or filter the row out
+			ELSE sls_quantity 
+		END AS sls_quantity,
+
+		CASE 
+			WHEN sls_sales IS NULL OR sls_sales <= 0 
+				 OR sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
 			ELSE sls_sales
 		END AS sls_sales,
-		ISNULL(sls_quantity,0),
+
 		CASE 
-			WHEN sls_price IS NULL OR sls_price<=0 OR sls_price!=ABS(NULLIF(sls_sales,0)/NULLIF(sls_quantity,0)) THEN ABS(NULLIF(sls_sales,0)/NULLIF(sls_quantity,0))
+			WHEN sls_price IS NULL OR sls_price <= 0 
+				 OR sls_price != ABS(sls_sales) / NULLIF(sls_quantity, 0)
+			THEN ABS(sls_sales) / NULLIF(sls_quantity, 0)
 			ELSE sls_price
 		END AS sls_price
 	FROM bronze.crm_sales_details;
